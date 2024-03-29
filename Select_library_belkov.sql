c11c0c940bb87717b0e9e312ddbfe2f72c283476
Dmitriy Belkov <belkovdmitriy1998@mail.ru>
2024-03-30 02:36:15 +0300

    RIGHT and LEFT JOINs replaced by JOIN, fixed checks for task No.2, used FIRST_VALUE

--- Select_library_belkov.sql
+++ Select_library_belkov.sql
@@ -2,9 +2,9 @@
 -- N - в формате даты
 create function books_return(in N date, out varchar(50))
 AS
-'select book_name from book
-right join
-book_list ON book.id_book = book_list.id_book where book_list.return_data = N'
+'select book_name from book_list
+join
+book ON book.id_book = book_list.id_book where book_list.return_data = N'
 language SQL;
 
 --посмотреть все даты для книг
@@ -13,20 +13,19 @@ language SQL;
 select books_return('2015-04-26');
 
 -- 2) вывести все книги из Book, которые брали в библиотеке больше одного раза, и у которых 1 автор.
-select book_name as название_книги, A3.reserved_count as количество_записей, A3.author_count as количество_авторов  from book
-right join(
-	select A1.id_book, A1.reserved_count, A2.author_count  from (
+select book_name as название_книги, A1.reserved_count as количество_записей, A2.author_count as количество_авторов  from (
 		select book_list.id_book, count(book_list.id_book) as reserved_count from book_list
 		group by book_list.id_book
-	) A1
-	left join(
+		having count(book_list.id_book) > 1
+	) A1 
+	join(
 		select author_book.id_book, count(author_book.id_book) as author_count from author_book
 		group by author_book.id_book
+		having count(author_book.id_book) = 1
 	) A2
 	on A1.id_book = A2.id_book
-	where reserved_count > 1 and author_count = 1
-) A3
-on A3.id_book = book.id_book;
+	join book
+	on A1.id_book = book.id_book;
 
 
 --3) найти Имя Employee для записи из record по конкретному Visitor на конкретный день, где паспорт Visitor = M.
@@ -35,32 +34,34 @@ as
 'select first_name as имя_сотрудника from person
 where id_person =
 (select id_employee from record
-left join 
+join 
 person on record.id_visitor = person.id_person
-left join
+join
 pasport_details on pasport_details.id_pasport = person.id_pasport
 where number = N and series = M and data::date = D)'
 language SQL;
 
 --посмотреть все даты записей и паспортные данные посетителей для них
 select data as дата_записи, pasport_details."number" as номер_пас_посетит, pasport_details.series as сер_пас_посетит from record
-left join 
+join 
 person on record.id_visitor = person.id_person
-left join
+join
 pasport_details on pasport_details.id_pasport = person.id_pasport;
 
 select employee_of('6048','21-18','2010-02-08');
 
 
 --4) в таблице author для каждой страны вывести самого младшего и самого старшего автора (использовать аналитические функции)
-select A2.country, A2.first_name||' '||A2.last_name as старший_автор, A2.birth_year, A3.first_name||' '||A3.last_name as младший_автор, A3.birth_year from author as A3
-right join
-(select A1.country, first_name, last_name, birth_year, A1.max_date from author as A2
-	right join(
-		select A1.country, min(birth_year) as min_date, max(birth_year) as max_date from author as A1
-				group by country
-			) A1
-	on A1.min_date=A2.birth_year and A1.country = A2.country) A2
-on A2.max_date = A3.birth_year and A2.country = A3.country;
+select distinct 
+	country,
+	first_value(first_name||' '||last_name) OVER (
+        PARTITION BY country ORDER BY birth_year ASC) as старший_автор,
+    first_value(birth_year) OVER (
+        PARTITION BY country ORDER BY birth_year ASC) as год_старший_автор,    
+    first_value(first_name||' '||last_name) OVER (
+        PARTITION BY country ORDER BY birth_year DESC) as младший_автор,
+    first_value(birth_year) OVER (
+        PARTITION BY country ORDER BY birth_year DESC) as год_младший_автор
+    from author;
 
 
